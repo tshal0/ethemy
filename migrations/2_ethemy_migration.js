1d76e757fc7088f04e32c01c7f1c06e4750c4fd9
valkn0t <twshal@gmail.com>
2017-11-05 14:09:15 -0600

    added migration stuff for truffle

--- migrations/2_ethemy_migration.js
+++ migrations/2_ethemy_migration.js
@@ -1,6 +1,8 @@
 var Ethemy = artifacts.require("./Ethemy.sol");
 var Dev = artifacts.require("./Dev.sol");
+var Lunyr = artifacts.require("./Lunyr.sol");
 module.exports = function(deployer) {
   deployer.deploy(Ethemy, {gas: 1000000});
   deployer.deploy(Dev, {gas: 1000000});
+  deployer.deploy(Lunyr, {gas: 1000000});
 };
